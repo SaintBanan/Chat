351309ee932f90ffd8785d7aafbe42dac4c52a19
SaintBanan <gumnew@mail.ru>
2018-11-07 19:38:07 +0700

    new

---
+++ client.js
@@ -0,0 +1,41 @@
+var		display = document.getElementById("display");
+var		input = document.getElementById("input");
+var		Status = document.getElementById("status");
+var 	nickname = null;
+var		nick = document.getElementById("nick");
+const	send = document.getElementById("send");
+const	client = new WebSocket("ws://192.168.1.8:3000");
+
+while(nickname == null || nickname == '' || nickname.length > 14)
+	nickname = prompt("Введите nickname:\n(не более 14-ти символов)");
+
+nick.innerHTML = nickname;
+
+function PrintMsg(msg)
+{
+	var i = 0;
+	
+	while(msg[i] != '!') i++;
+	
+	var user = msg.substring(0, i);
+	
+	msg = msg.substring(i + 1);	
+	
+	display.value += "Сообщение от " + user + '\n' + msg + "\n\n";
+}
+
+client.onopen = () => Status.innerHTML = "Online";
+client.onclose = () => Status.innerHTML = "Offline";
+client.onmessage = msg => PrintMsg(msg.data);
+
+send.addEventListener("click", e => {
+	if(input.value != '')
+	{
+		var user = nickname + '!';
+		var msg = user.concat(input.value);
+		
+		client.send(msg);
+		PrintMsg(msg);
+		input.value = '';
+	}
+});
---
+++ welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="welcome.css">
+		<link rel="shortcut icon" type="image/jpg" href="favicon.jpg">
+		<title>CHAT VAndreyka</title>
+	</head>
+	
+	<body>
+		<div class="form">
+			<form action="client.php" method="POST">
+				<input type="text" name="nick">
+				<input type="submit">
+			</form>
+		</div>
+	</body>
+</html>
