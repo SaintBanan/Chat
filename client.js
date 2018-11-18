3ccd39f59ff38a41dbc5968899965ae5b75bba93
SaintBanan <gumnew@mail.ru>
2018-11-18 18:24:05 +0700

    new

--- client.js
+++ client.js
@@ -1,25 +1,59 @@
-var	display = document.getElementById("display");
-var	input = document.getElementById("input");
-var	send = document.getElementById("send");
-var	client = new WebSocket("ws://localhost:3000");
+var		display = document.getElementById("display");
+var		input = document.getElementById("input");
+var		Status = document.getElementById("status");
+var 	nickname = '';
+var		nick = document.getElementById("nick");
+var		send = document.getElementById("send");
+var		host = location.origin.replace("http", "ws");
+const	client = new WebSocket(host);
 
-function PrintMsg(msg)
+while(nickname == null || nickname == '' || nickname.length > 14)
+	nickname = prompt("Введите nickname:\n(не более 14-ти символов)");
+
+nick.innerHTML = nickname;
+
+function PrintMsg(msg, flag)
 {	
-	var data = JSON.parse(msg.data);
-
-	display.value += data.message + "\n\n";
+	var tmp = msg;
+	
+	if(flag) tmp = msg.data;
+	
+	var data = JSON.parse(tmp);
+	var message = data.message;
+	var index = -1;
+	
+	while((index = message.indexOf('\n', index + 1)) != -1 && index < message.length)
+	{
+		var SecondIndex = index + 1;
+		
+		if(message[SecondIndex] == '\n' && SecondIndex < message.length)
+		{
+			while(message[SecondIndex] == '\n') SecondIndex++;
+			
+			var tmp = message.substring(index, SecondIndex);
+			
+			message = message.replace(tmp, '\n');
+		}
+	}
+	
+	if(message[message.length - 1] == '\n') message = message.substring(0, message.length - 1);
+	if(message[0] == '\n') message = message.substring(1);
+	if(message.length != 0) display.value += "Сообщение от " + data.user + '\n' + message + "\n\n";
 }
 
-client.onopen = () => alert("online");
-client.onclose = () => alert("offline");
-client.onmessage = msg => PrintMsg(msg);
+client.onopen = () => Status.innerHTML = "Online";
+client.onclose = () => Status.innerHTML = "Offline";
+client.onmessage = msg => PrintMsg(msg, true);
 
 send.addEventListener("click", e => {
+	e.preventDefault();
+	
 	if(input.value != '')
 	{
-		var js = JSON.stringify({ message: input.value});
+		var js = JSON.stringify({ message: input.value, user: nickname });
 		
 		client.send(js);
+		PrintMsg(js, false);
 		input.value = '';
 	}
 });
--- server.js
+++ server.js
@@ -1,12 +1,49 @@
-var	WebSocket = require("ws");
-var server = new WebSocket.Server({ port: 3000 });
+const express = require("express");
+const http_server = express();
+const port = process.env.PORT || 3000;
+const file = require("fs");
+
+http_server.get('/', (req, res) => {
+	res.writeHead(200, { "Content-Type": "text/html" });
+	file.readFile("client.html", (err, data) => res.end(data));
+});
+
+http_server.get('/client.css', (req, res) => {
+	res.writeHead(200, { "Content-Type": "text/css" });
+	file.readFile("client.css", (err, data) => res.end(data));
+});
+
+http_server.get('/client.js', (req, res) => {
+	res.writeHead(200, { "Content-Type": "text/javascript" });
+	file.readFile("client.js", (err, data) => res.end(data));
+});
+
+http_server.get('/chat.jpeg', (req, res) => {
+	res.writeHead(200, { "Content-Type": "image/jpeg" });
+	file.readFile("chat.jpeg", (err, data) => res.end(data) );
+});
+
+http_server.get('/back.jpg', (req, res) => {
+	res.writeHead(200, { "Content-Type": "image/jpg" });
+	file.readFile("back.jpg", (err, data) => res.end(data));
+});
+
+http_server.get('/favicon.jpg', (req, res) => {
+	res.writeHead(200, { "Content-Type": "image/x-icon" });
+	file.readFile("favicon.jpg", (err, data) => res.end(data));
+});
+
+http_server.listen(port, () => console.log("Сервер работает..."));
+
+const	WebSocket = require("ws").Server;
+var 	server = new WebSocket({ server: http_server });
 
 server.on("connection", ws => {
 	
 	ws.on("message", msg => {	
 		server.clients.forEach(client => {
 			
-			if(client.readyState === WebSocket.OPEN) client.send(msg);
+			if(client.readyState === WebSocket.OPEN && client != ws) client.send(msg);
 		});
 	});
 });
