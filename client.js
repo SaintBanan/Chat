d5f0104ed892d20eaa2269fd4a9eae1b8453e096
SaintBanan <gumnew@mail.ru>
2018-11-19 01:14:49 +0700

    new

--- client.js
+++ client.js
@@ -4,56 +4,58 @@ var		Status = document.getElementById("status");
 var 	nickname = '';
 var		nick = document.getElementById("nick");
 var		send = document.getElementById("send");
-var		host = location.origin;
-const	client = io.connect(host);
+const	client = io.connect(location.origin);
 
 while(nickname == null || nickname == '' || nickname.length > 14)
 	nickname = prompt("Введите nickname:\n(не более 14-ти символов)");
 
 nick.innerHTML = nickname;
 
-function PrintMsg(message, flag)
+function PrintMsg(msg)
 {	
-	/*var tmp = msg;
+	var message = msg.message;
 	
-	if(flag) tmp = msg.data;
+	message = DelElem(message, '\n');
+	message = DelElem(message, ' ');
 	
-	var data = JSON.parse(tmp);
-	var message = data.message;
-	var index = -1;*/
-	console.log(message);
-	/*while((index = message.indexOf('\n', index + 1)) != -1 && index < message.length)
-	{
-		var SecondIndex = index + 1;
-		
-		if(message[SecondIndex] == '\n' && SecondIndex < message.length)
-		{
-			while(message[SecondIndex] == '\n') SecondIndex++;
-			
-			var tmp = message.substring(index, SecondIndex);
-			
-			message = message.replace(tmp, '\n');
-		}
-	}
-	
-	if(message[message.length - 1] == '\n') message = message.substring(0, message.length - 1);
-	if(message[0] == '\n') message = message.substring(1);*/
-	if(message.length != 0) display.value += message + "\n\n";//"Сообщение от " + data.user + '\n' + message + "\n\n";
+	if(message.length != 0) display.value += "Сообщение от " + msg.user + '\n' + message + "\n\n";
 }
 
 client.on("connect", () => Status.innerHTML = "Online");
 client.on("disconnect", () => Status.innerHTML = "Offline");
-client.on("msg", data => PrintMsg(data.message, true) );
+client.on("msg", msg => PrintMsg(msg) );
 
 send.addEventListener("click", e => {
-	//e.preventDefault();
-	
 	if(input.value != '')
 	{
-		//var js = JSON.stringify({ message: input.value, user: nickname });
+		var msg = { message: input.value, user: nickname };
 		
-		client.emit("msg", { message: input.value });
-		PrintMsg(input.value, false);
+		client.emit("msg", msg);
+		PrintMsg(msg);
 		input.value = '';
 	}
 });
+
+function DelElem(message, elem)
+{
+	var index = -1;
+	
+	while((index = message.indexOf(elem, index + 1)) != -1)
+	{
+		var SecondIndex = index + 1;
+		
+		if(message[SecondIndex] == elem && SecondIndex < message.length)
+		{
+			while(message[SecondIndex] == elem) SecondIndex++;
+			
+			var tmp = message.substring(index, SecondIndex);
+			
+			message = message.replace(tmp, elem);
+		}
+	}
+	
+	if(message[message.length - 1] == elem && message.length != 0) message = message.substring(0, message.length - 1);
+	if(message[0] == elem) message = message.substring(1);
+	
+	return message;
+}
--- server.js
+++ server.js
@@ -12,14 +12,5 @@ hServer.listen(port, () => console.log("Сервер работает..."));
 
 server.on("connection", ws => {
 	
-	/*ws.on("message", msg => {	
-		server.clients.forEach(client => {
-			
-			if(client.readyState === WebSocket.OPEN && client != ws) client.send(msg);
-		});
-	});*/
-	
-	//ws.on("disconnect", () => console.log("Client disconnected(") );
-	
 	ws.on("msg", msg => ws.broadcast.emit("msg", msg));
 });
