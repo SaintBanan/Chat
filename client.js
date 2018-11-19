08022427835c44bee7772e4867de7c33bbb1f2c9
SaintBanan <gumnew@mail.ru>
2018-11-20 00:29:41 +0700

    new

--- client.js
+++ client.js
@@ -6,6 +6,27 @@ var		nick = document.getElementById("nick");
 var		send = document.getElementById("send");
 const	client = io.connect(location.origin);
 
+//установка ширины и высоты приложения относительно разрешения экрана устройства
+
+var width = (window.screen.width - 18);
+
+if(width <= 480)
+{
+	var height = (window.screen.height - 100);
+	var _form = document.getElementsByClassName("form");
+
+	_form[0].style.width = width.toString() + "px";
+	_form[0].style.height = height.toString() + "px";
+
+	display.style.width = (width - 20).toString() + "px";
+	display.style.height = (height - 80).toString() + "px";
+
+	input.style.width = (width - 130).toString() + "px";
+	send.style.width = ((width - 20) - (width - 110)).toString() + "px";
+}
+
+//установка...
+
 while(nickname == null || nickname == '' || nickname.length > 14)
 	nickname = prompt("Введите nickname:\n(не более 14-ти символов)");
 
@@ -52,10 +73,19 @@ function DelElem(message, elem)
 			
 			message = message.replace(tmp, elem);
 		}
+		
+		if(message[index - 1] == '\n' && index != 0)
+		{
+			var endStr = message.substring(index + 1);
+			var startStr = message.substring(0, index);
+			message = startStr.concat(endStr);
+		}
 	}
 	
-	if(message[message.length - 1] == elem && message.length != 0) message = message.substring(0, message.length - 1);
+	if(message[message.length - 1] == elem) message = message.substring(0, message.length - 1);
 	if(message[0] == elem) message = message.substring(1);
+	
+	message = message.trim();
 	
 	return message;
 }
